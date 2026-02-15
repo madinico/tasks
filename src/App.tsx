a6e3523509f6831d4a6632423ecd2b0db90ee87c
madinico <madinico@udel.edu>
2026-02-15 18:58:37 -0500

    Added an image, a button, and a list.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -8,9 +9,34 @@ function App(): React.JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
                 <h3>Madison Saunders</h3>
             </header>
+            <header>Stardew Valley</header>
+
+            <img
+                src="../images/StardewLogo.jpg"
+                alt="The Stardew Valley start screen"
+            />
+
+            <h4>Crops grown in the Spring</h4>
+            <ul>
+                <li>Cauliflower</li>
+                <li>Potato</li>
+                <li>Carrot</li>
+                <li>Parsnip</li>
+                <li>Kale</li>
+            </ul>
+            <div>
+                <Button
+                    onClick={() => {
+                        console.log("Hello World!");
+                    }}
+                >
+                    Log Hello World
+                </Button>
+            </div>
+            <hr></hr>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. <br></br> Hello World!
+                automatically reload. <br></br>
             </p>
         </div>
     );
