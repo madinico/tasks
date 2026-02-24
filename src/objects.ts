d78b525d43e7c5f0afce0f9f54f65cd0150cd0b3
madinico <madinico@udel.edu>
2026-02-24 12:24:46 -0500

    Completed publishQuestion Function

--- src/objects.ts
+++ src/objects.ts
@@ -109,7 +109,13 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    const newQuestion = { ...question };
+    if (newQuestion.published) {
+        newQuestion.published = false;
+    } else {
+        newQuestion.published = true;
+    }
+    return newQuestion;
 }
 
 /**
