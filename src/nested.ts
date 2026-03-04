784af68ef92ce784fb05ef8a6f51c6b96c704f3b
madinico <madinico@udel.edu>
2026-03-04 17:00:40 -0500

    Finished all functions in duplicateQuestionInArray

--- src/nested.ts
+++ src/nested.ts
@@ -1,6 +1,6 @@
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
-import { makeBlankQuestion } from "./objects";
+import { makeBlankQuestion, duplicateQuestion } from "./objects";
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
@@ -246,7 +246,19 @@ export function editOption(
     targetOptionIndex: number,
     newOption: string,
 ): Question[] {
-    return [];
+    const newQuestions = questions.map((question: Question): Question => {
+        if (question.id === targetId) {
+            const newOptions = [...question.options];
+            if (targetOptionIndex === -1) {
+                newOptions.push(newOption);
+            } else {
+                newOptions[targetOptionIndex] = newOption;
+            }
+            return { ...question, options: newOptions };
+        }
+        return { ...question };
+    });
+    return newQuestions;
 }
 
 /***
@@ -260,5 +272,14 @@ export function duplicateQuestionInArray(
     targetId: number,
     newId: number,
 ): Question[] {
-    return [];
+    const foundQuestionIndex: number = questions.findIndex(
+        (question: Question): boolean => question.id === targetId,
+    );
+    const newQuestions = [...questions];
+    newQuestions.splice(
+        1 + foundQuestionIndex,
+        0,
+        duplicateQuestion(newId, newQuestions[foundQuestionIndex]),
+    );
+    return newQuestions;
 }
