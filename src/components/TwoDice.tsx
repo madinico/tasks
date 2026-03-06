700c28d4d1f7b1e121fe662aeb30f0dd3ab6123e
madinico <madinico@udel.edu>
2026-03-06 12:57:30 -0500

    Completed TwoDice Component

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -11,6 +11,66 @@ export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
+/**
+ * The TwoDice component will simulate a game where you
+ * roll two dice in an attempt to get matching values.
+ *
+ * However, you lose the game if your dice ever come up as
+ * a pair of ones (“snake eyes”). You will need two states, one for each die.
+ * Each die’s value should be rendered in the View in a span tag of their own,
+ * with the first dice having the data-testid of left-die and
+ * the second dice having the data-testid of right-die.
+ *
+ * You will need two “Roll” buttons (labelled Roll Left and Roll Right).
+ * Clicking a Roll button will change the value for the
+ * corresponding dice using the provided d6 function.
+ *
+ * The initial values of the dice cannot be the same.
+ * When the two states are equal but ARE both ones,
+ * render a message that includes the word Lose.
+ *
+ * When the two states are equal but are NOT both ones,
+ * render a message that includes the word Win.
+ *
+ * If you do all these and are still not passing all tests,
+ * read the test file, it is a good practice!
+ *
+ * In much of software engineering, the tests are the product spec.
+ */
+
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDice, setLeftDice] = useState<number>(2);
+    const [rightDice, setRightDice] = useState<number>(5);
+
+    return (
+        <div>
+            <div>
+                <Button
+                    onClick={() => {
+                        setLeftDice(d6());
+                    }}
+                >
+                    Roll Left
+                </Button>
+                <span data-testid="left-die">{leftDice}</span>
+            </div>
+            <div>
+                <Button
+                    onClick={() => {
+                        setRightDice(d6());
+                    }}
+                >
+                    Roll Right
+                </Button>
+                <span data-testid="right-die">{rightDice}</span>
+            </div>
+            <div>
+                {leftDice === 1 && rightDice === 1 ?
+                    "LOSE"
+                : leftDice === rightDice ?
+                    "WIN"
+                :   ""}
+            </div>
+        </div>
+    );
 }
