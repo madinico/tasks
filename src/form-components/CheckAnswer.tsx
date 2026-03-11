2853fd8d919fca7685ee4a22a1cbf0e4d334da12
madinico <madinico@udel.edu>
2026-03-11 12:42:55 -0400

    Completed CheckAnswer Component

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
+type eventType = React.ChangeEvent<HTMLInputElement>;
 export function CheckAnswer({
     expectedAnswer,
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+    function updateAnswer(event: eventType) {
+        setAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="textbox"></Form.Group>
+            <Form.Label>
+                <h3> Check Answer: </h3>
+            </Form.Label>
+            <Form.Control value={answer} onChange={updateAnswer} />
+            {expectedAnswer === answer ? "✔️" : "❌"}
         </div>
     );
 }
