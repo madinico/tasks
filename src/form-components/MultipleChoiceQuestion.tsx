3a4939c38750e2f653f086adb02a2063d5f9722a
madinico <madinico@udel.edu>
2026-03-11 22:26:41 -0400

    Completed MultipleChoiceQuestion Component

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+type eventType = React.ChangeEvent<HTMLSelectElement>;
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +10,25 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [choice, setChoice] = useState<string>(options[0]);
+
+    function updateChoice(event: eventType) {
+        setChoice(event.target.value);
+    }
+
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="multipleChoice">
+                <Form.Label>Multiple Choice Question</Form.Label>
+                <Form.Select value={choice} onChange={updateChoice}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{expectedAnswer === choice ? "✔️" : "❌"}</div>
         </div>
     );
 }
